d008f8dd84c80d0bb1f4d34b0a4a12af4354d273
Paola Mateo <paolamateo@Paolas-Air.lan>
2021-10-02 19:21:35 -0500

    file edit

---
+++ PyBank/main.py
@@ -0,0 +1,77 @@
+# module for reading csv files
+
+import os
+import csv
+from pathlib import Path
+
+# Set path for files
+budget_data_csv=os.path.join("/Users/paolamateo/smu-dal-data-pt-09-2021-u-c/02-Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv")
+
+#Declare variables
+total_months = []
+total_profit = []
+profit_changes = 0
+monthly_profit_change=[]
+previous_value =0
+
+
+# Open the CSV file
+with open(budget_data_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Skip the header or otherwise will count it as another month
+    csv_reader=next(csvreader)
+
+    for row in csvreader:
+
+#Total of months and profits/losses
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+
+    print(len(total_months))
+
+# The net total amount of "Profit/Losses" over the entire period
+
+total_profits=[int(x) for x in total_profit]
+total_profits_sum=sum(total_profits)
+print (total_profits_sum) 
+
+# Changes in "Profit/Losses" over the entire period
+
+for i in range(len(total_profits)-1):
+
+    # Difference between two months and append to monthly profit change
+    monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+
+# Obtain the max and min of the monthly profit change list
+
+max_increase_value= max(monthly_profit_change)
+max_decrease_value =min(monthly_profit_change)
+
+# Correlate max and min to the proper month list and index from max and min 
+# Use plus 1 at the end since month associated with change is the next month
+
+max_increase_month=monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month= monthly_profit_change.index(min(monthly_profit_change)) + 1
+
+#Output files
+
+output_file = os.path.join("financial_analysis_summary.txt")
+
+with open(output_file, "w") as file:
+
+# Write methods to print to Analysis
+
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("-------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits:{total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write("\n")
+    file.write (f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
---
+++ PyPoll/main.py
@@ -0,0 +1,102 @@
+# Module for reading csv file
+
+import os
+import csv
+from pathlib import Path 
+
+#Set path for files
+election_data_csv=os.path.join("/Users/paolamateo/Downloads/GitHub/Repository/python-challenge/PyPoll/Resources/election_data.csv")
+
+# Declare variables
+total_votes=0
+k_votes=0
+c_votes=0
+li_votes=0
+o_votes=0
+
+# Open csv in default mode 
+with open (election_data_csv, newline="", encoding="utf-8") as elections:
+
+    #Store data under the csv reader variable
+    csvreader=csv.reader(elections, delimiter=",")
+
+    #Skip the header to get to actual values
+    header=next(csvreader)
+    #Go through each row in csv
+    for row in csvreader:
+
+            #Count voter ID and create variable
+            total_votes +=1
+
+            #Count times that name appears and store in list
+            if row[2]== "Khan":
+                k_votes +=1
+            elif row[2] == "Correy":
+                c_votes +=1
+            elif row[2] =="Li":
+                li_votes +=1
+            elif row[2] == "O'Tooley":
+                o_votes +=1
+
+
+# Dictionary to hold candidates names
+
+candidates =[
+    "Khan",
+    "Li", 
+    "Correy", 
+    "O'Tooley"]
+
+# Dictionary to hold votes 
+votes = [
+    k_votes,
+    c_votes,
+    li_votes,
+    o_votes]
+
+#Zip the list of candidate (key) and the total votes (value)
+dict_candidates_and_votes=dict(zip(candidates,votes))
+
+#Find winner with max function
+key= max (dict_candidates_and_votes, 
+key=dict_candidates_and_votes.get)
+
+
+#Print summary of analysis
+khan_percent = (k_votes/total_votes) *100
+correy_percent = (c_votes/total_votes) *100
+li_percent = (li_votes/total_votes) *100
+otooley_percent = (o_votes/total_votes) *100
+
+
+
+# Output files
+output_file= os.path.join("poll_analysis_summary.txt")
+
+with open(output_file, "w") as file:
+
+#Write methods to print to Analysis
+
+    file.write ("Election Results")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Votes: {total_votes}")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Khan: {khan_percent: .3f}% ({k_votes})")
+    file.write("\n")
+    file.write(f"Correy:{correy_percent: .3f}% ({c_votes})")
+    file.write("\n")
+    file.write(f"Li: {li_percent: .3f}% ({li_votes})")
+    file.write("\n")
+    file.write(f"O'Tooley: {otooley_percent:.3f}% ({o_votes})")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Winner: {key}")
+    file.write("\n")
+    file.write(f"---------------------------")
+
+
